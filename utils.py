571642c6578ec0a219ead9d5a4971c8fcf68c831
Guilherme de Morais Richter <guilhermemoraisr@gmail.com>
2022-09-04 15:47:53 -0300

    Adicionando arquivo utils, que possui funções auxiliares para verificar posições para os agentes
    Co-authored-by: italovinicius18 <italovinicius18@yahoo.com>
    Co-authored-by: IanFPFerreira <ianfillipe@gmail.com>
    Co-authored-by: gabrielbpn <gabrielbpn0802@gmail.com>
    Co-authored-by: AlvaroLeles <alvaro.leles@hotmail.com>
    Co-authored-by: castroricardo1 <ricardoloureiro75@gmail.com>
    Co-authored-by: alvesitalo  <italooko@gmail.com>

---
+++ utils.py
@@ -0,0 +1,24 @@
+"""
+    Funções auxiliares para verificar posições para os Agentes
+"""
+
+
+def posicaoVazia(self, posicoes_ocupadas):
+    x = self.random.randrange(self.grid.width)
+    y = self.random.randrange(self.grid.height)
+    pos = (x, y)
+
+    # Verifica se a posição já está ocupada
+    while posicaoJaExiste(pos, posicoes_ocupadas):
+        x = self.random.randrange(self.grid.width)
+        y = self.random.randrange(self.grid.height)
+        pos = (x, y)
+
+    return pos
+
+
+def posicaoJaExiste(pos, lista):
+    for item in lista:
+        if item == pos:
+            return True
+    return False
